4d736561772acd904da82fc1f2ffada27d3cfcd0
Luiz Santos <lcsjr.s23@gmail.com>
2022-04-18 23:50:09 -0300

    feature: initial implementation webClient with Circuit-Breaker

---
+++ src/main/java/com/study/wccb/MsWebclientCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.study.wccb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWebclientCircuitBreakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsWebclientCircuitBreakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/wccb/MsWebclientCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.wccb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWebclientCircuitBreakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
