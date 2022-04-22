ac4671391c5535f650d3fa627fd9f2d50acd0591
Luiz Santos <lcsjr.s23@gmail.com>
2022-04-22 03:16:13 -0300

    feature: template jmeter and refactory bean webclient

--- src/main/java/com/study/wccb/config/DefaultWebClientConfig.java
+++ src/main/java/com/study/wccb/config/DefaultWebClientConfig.java
@@ -1,33 +1,25 @@
 package com.study.wccb.config;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class DefaultWebClientConfig {
 
     @Bean
-    public WebClient webClient(){
+    public WebClient webClientPlaceHolder(){
 
-        final HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-                .responseTimeout(Duration.ofMillis(5000))
-                .doOnConnected(conn ->
-                        conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
-
-        return WebClient.builder()
-            .clientConnector(new ReactorClientHttpConnector(httpClient))
-            .build();
+        return WebClient
+                .builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .exchangeStrategies(
+                        ExchangeStrategies.builder().codecs(c->c.defaultCodecs().enableLoggingRequestDetails(true)).build()
+                )
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
--- src/main/java/com/study/wccb/controller/ExampleController.java
+++ src/main/java/com/study/wccb/controller/ExampleController.java
@@ -3,12 +3,14 @@ package com.study.wccb.controller;
 import com.study.wccb.model.Post;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.reactivestreams.Publisher;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -20,42 +22,29 @@ import java.net.URI;
 @RequestMapping(path = "/v1/example", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ExampleController {
 
-    private final WebClient webClient;
+    private final WebClient webClientPlaceHolder;
 
-    @GetMapping(path = "/users/{name}")
-    public Mono<String> getName(@PathVariable String name) {
-        return Mono.just(name);
+   // @GetMapping(path = "/users/{name}")
+    public Mono<ServerResponse> getName(@PathVariable String name) {
+        String frase = "Olá " + name +"! Tudo bem ?";
+        return ServerResponse.ok().body(frase, String.class);
     }
 
     @GetMapping("/{id}")
     public Mono<Post> getId(@PathVariable String id){
-
-        // "https://jsonplaceholder.typicode.com/posts/1"
-
-        log.info("### We wrap a single Employee resource in a Mono because we return at most one employee");
-        Mono<Post> postMono = webClient.get()
-                .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1"))
-                .retrieve()
-                .bodyToMono(Post.class);
-
-        //postMono.subscribe(System.out::println);
-        return postMono;
+        log.info("### [MONO] Buscando um unico objeto");
+        return webClientPlaceHolder
+                .get().uri("/posts/{id}", id)
+                .retrieve().bodyToMono(Post.class);
 
     }
 
     @GetMapping
     public Flux<Post> getAll(){
+        log.info("### [FLUX] Buscando uma lista de objetos");
+        return webClientPlaceHolder
+                .get().uri("/posts" )
+                .retrieve().bodyToFlux(Post.class);
 
-        // "https://jsonplaceholder.typicode.com/posts"
-
-        log.info("### For the collection resource, we use a Flux of type Post since that's the publisher for 0..n elements. ");
-
-        Flux<Post> postFlux = webClient.get()
-                .uri(URI.create("https://jsonplaceholder.typicode.com/posts" ))
-                .retrieve()
-                .bodyToFlux(Post.class);
-
-        //postFlux.subscribe(System.out::println);
-        return postFlux;
     }
 }
